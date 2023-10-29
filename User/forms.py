6363d9d1dcd521394ebc9a635f71365fff6e8ea8
Kristéan Emmanuel <kebeniga@gmail.com>
2023-10-29 21:13:17 +0800

    Added custom user model Added user app for everything user-related Added login and registration Added email services (confirmation email) Added database connectivity using postgres Modified project settings to use .env variables for security

---
+++ User/forms.py
@@ -0,0 +1,42 @@
+from django import forms
+from django.contrib.auth import password_validation
+from django.contrib.auth.forms import AuthenticationForm
+from django.core.exceptions import ValidationError
+from .models import CustomUser
+
+
+class UserRegistrationForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput)
+    password_confirm = forms.CharField(widget=forms.PasswordInput, label="Confirm Password")
+
+    class Meta:
+        model = CustomUser
+        fields = ['username', 'email', 'password', 'password_confirm', 'first_name', 'last_name']
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get('password')
+        password_confirm = cleaned_data.get('password_confirm')
+
+        if password and password_confirm and password != password_confirm:
+            raise forms.ValidationError("Passwords do not match")
+
+    def clean_password(self):
+        password = self.cleaned_data.get('password')
+        try:
+            password_validation.validate_password(password, self.instance)
+        except ValidationError as e:
+            self.add_error('password', e)
+        return password
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        if CustomUser.objects.filter(email=email).exists():
+            raise forms.ValidationError("This email already exists.")
+        return email
+
+
+class UserLoginForm(AuthenticationForm):
+    class Meta:
+        model = CustomUser
+        fields = ('username', 'password')
---
+++ User/models.py
@@ -0,0 +1,53 @@
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.db import models
+
+
+# Create your models here.
+class CustomUserManager(BaseUserManager):
+    def create_user(self, username, email, password, **extra_fields):
+        if not username:
+            raise ValueError('The Username field must be set')
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(username=username, email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(username, email, password, **extra_fields)
+
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(max_length=30, unique=True)
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(auto_now_add=True)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email', 'first_name', 'last_name']
+
+    def __str__(self):
+        return self.username
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    points = models.PositiveIntegerField(default=0)
+
+
+class UserRanking(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    category = models.CharField(max_length=100)  # e.g., "NFA" "DFA" "RegEx"
+    ranking_value = models.PositiveIntegerField()
+
+    class Meta:
+        unique_together = ('user', 'category')
