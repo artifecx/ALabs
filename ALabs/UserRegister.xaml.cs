84713bb0fd4e0c819ecff04eb46de2340d7d131f
Jose Elijah Fernandez <fjelijah215@gmail.com>
2024-01-11 18:47:57 +0800

    User registration completed
    unique key logic not yet implemented

--- ALabs/UserRegister.xaml.cs
+++ ALabs/UserRegister.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ALabs.Database;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -55,14 +56,14 @@ namespace ALabs
 
         private void NameBox_GotFocus(object sender, RoutedEventArgs e)
         {
-            PasswordPlaceholder.Visibility = Visibility.Collapsed;
+            NamePlaceholder.Visibility = Visibility.Collapsed;
         }
 
         private void NameTextBox_LostFocus(object sender, RoutedEventArgs e)
         {
-            if (string.IsNullOrEmpty(PasswordBox.Password))
+            if (string.IsNullOrEmpty(NameBox.Text))
             {
-                PasswordPlaceholder.Visibility = Visibility.Visible;
+                NamePlaceholder.Visibility = Visibility.Visible;
             }
         }
 
@@ -73,7 +74,39 @@ namespace ALabs
 
         private void RegisterButton_Click(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("Register button clicked!");
+            var username = UsernameTextBox.Text;
+            var password = PasswordBox.Password;
+            var name = NameBox.Text;
+
+            if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password) || string.IsNullOrEmpty(name))
+            {
+                MessageBox.Show("Please enter all required information.");
+                return;
+            }
+
+            using (UserDataContext context = new UserDataContext())
+            {
+                // Check if the username already exists
+                if (context.Users.Any(user => user.Username == username))
+                {
+                    MessageBox.Show("Username already exists. Please choose a different username.");
+                    return;
+                }
+
+                // Create a new user and save it to the database
+                User newUser = new User
+                {
+                    Username = username,
+                    Password = password,
+                    Name = name
+                };
+
+                context.Users.Add(newUser);
+                context.SaveChanges();
+
+                MessageBox.Show("Registration successful!");
+                mainWindow.mainFrame.Navigate(new UserLogin(mainWindow));
+            }
         }
     }
 }
